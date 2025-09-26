a67fd77f1a267d4457442cb561e0dba950196fce
lace <la171468@ucf.edu>
2025-09-26 13:15:26 -0400

    ^

--- public/main.js
+++ public/main.js
@@ -1,6 +1,19 @@
 const draggable = document.querySelector(".draggable");
 
+let isDragging = false;
+
+draggable.addEventListener("mousedown", (event) => {
+    isDragging = true;
+})
+
 document.addEventListener("mousemove", (event) =>{
-    draggable.style.left = `${event.clientX}px`
-    draggable.style.top = `${event.clientY}px`
+    if (isDragging){
+        draggable.style.left = `${event.clientX}px`
+        draggable.style.top = `${event.clientY}px`
+    }
+    
+})
+
+document.addEventListener("mouseup", (event) => {
+    isDragging = false;
 })
