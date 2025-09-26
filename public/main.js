82b734f745a64c1198a5813c99b5be72d741baee
lace <la171468@ucf.edu>
2025-09-25 23:28:54 -0400

    testing button

--- public/main.js
+++ public/main.js
@@ -1,2 +1,19 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Welcome to my page ${myName}`;
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Welcome to my page ${myName}`;
+}
+
+myButton.addEventListener("click", () => {
+    setUserName();
+});
