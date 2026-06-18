dcb1ef603074245597c4a3956533379aa64afab5
lace <la171468@ucf.edu>
2026-06-18 17:21:52 -0400

    starting other pages

--- public/layout.js
+++ public/layout.js
@@ -52,7 +52,7 @@ function headerHTML() {
           <div class="sidebar-title">Navigation</div>
           <ul>
             <li><a href="/">Home</a></li>
-            <li><a href="/page1">Page 1</a></li>
+            <li><a href="${nesting}/about">About</a></li>
             <li><a href="/page2">Page 2</a></li>
             <li><a href="/page3">Page 3</a></li>
         	<li>
